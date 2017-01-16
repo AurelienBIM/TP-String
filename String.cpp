b90e98c55def924609bc4b423ab4651e97bff171
RagondinMystique <gaetan.desanges@yahoo.fr>
2017-01-16 17:09:26 +0100

    Eh poto, j'ai màj le constructeur par copie !

--- String.cpp
+++ String.cpp
@@ -9,8 +9,23 @@ String::String() {
 }
 
 String::String( char* c ) {
-
+	length_ = length(c);
+	if (length_ < INIT_CAPACITY) {
+		chaine_ = new char[INIT_CAPACITY];
+		capacity_=INIT_CAPACITY;
+		for (int i=0; i<= length_;i+=1) {
+			chaine_[i] = c[i];
+		}
+	}
+	else {
+		chaine_ = new char[length_+1];
+		capacity_=length_+1;
+		for (int i=0; i<= length_;i+=1) {
+			chaine_[i] = c[i];
+		}
+	}
 }
+
 
 // String::String( String s ) { }
 
